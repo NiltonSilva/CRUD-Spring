b2c07590774120d0719240f89b64f71e50d482c1
nsfge <nsfgeo@hotmail.com>
2024-06-30 18:31:38 -0300

    feat: criado método para salvar curso

--- src/main/java/com/nilton/crud_spring/controller/CourseController.java
+++ src/main/java/com/nilton/crud_spring/controller/CourseController.java
@@ -2,14 +2,21 @@ package com.nilton.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nilton.crud_spring.model.Course;
 import com.nilton.crud_spring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -19,8 +26,14 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping()
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 
 }
